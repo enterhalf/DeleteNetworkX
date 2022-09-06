27bf3590d68b37e5c97a55060fe6a9481f9979c0
test <test@R7000-ZLZ1QWE>
2022-09-06 19:49:55 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Text.RegularExpressions;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace MyApp {
+    static class Program {
+        [DllImport("kernel32.dll")]
+        public static extern bool AllocConsole();
+
+        [DllImport("kernel32.dll")]
+        public static extern bool FreeConsole();
+
+        [STAThread]
+        private static void Main() {
+            _ = AllocConsole();
+
+            //检查是否是以管理员启动
+            if (!IsRunAsAdmin()) {
+                //创建启动对象
+                var startInfo = new ProcessStartInfo {
+                    UseShellExecute = true,
+                    WorkingDirectory = AppDomain.CurrentDomain.BaseDirectory,
+                    FileName =
+                        Process.GetCurrentProcess().MainModule.FileName ??
+                        throw new InvalidOperationException("获取本进程的文件位置失败"),
+                    Verb = "runas",
+                };
+                _ = Process.Start(startInfo);
+
+                return;
+            }
+
+            //上面是申请权限的代码
+
+
+            String path1 = "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\NetworkList\\Profiles";
+            String path2 = "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\NetworkList\\Signatures\\Unmanaged";
+            String deletePattern = "网络";
+
+            Reg reg1 = new Reg(path1, deletePattern);
+            reg1.Delete();
+            Reg reg2 = new Reg(path2, deletePattern);
+            reg2.Delete();
+
+
+        }
+        private static bool IsRunAsAdmin() {
+            //检查本程序是否是以Windows管理员身份运行
+            using var wi = WindowsIdentity.GetCurrent();
+            var wp = new WindowsPrincipal(wi);
+
+            return wp.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+    }
+    
+}
---
+++ Reg.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Win32;
+using System.Text.RegularExpressions;
+
+public class Reg {
+    public String path;
+    public String deletePattern;
+    public String[] deleteList;
+
+    public Reg(string path, string deletePattern) {
+        this.path = path;
+        this.deletePattern = deletePattern;
+    }
+
+    public void GenList() {
+        RegistryKey _ = Registry.LocalMachine;
+        RegistryKey rk = _.OpenSubKey(path);
+        deleteList = rk.GetSubKeyNames();
+    }
+
+    public void DeleteList() {
+        foreach (String key in deleteList) {
+            String keyname = $"HKEY_LOCAL_MACHINE\\{path}\\{key}";
+            var res = Registry.GetValue(keyname, "Description", null);
+
+            //若是有网络
+            if (Regex.IsMatch((String)res, deletePattern)) {
+                RegistryKey _ = Registry.LocalMachine;
+                RegistryKey rk = _.OpenSubKey(path, true);
+                rk.DeleteSubKey(key);
+                Console.WriteLine($"{key}已删除");
+            }
+
+        }
+    }
+    public void Delete() {
+        GenList();
+        DeleteList();
+    }
+}
